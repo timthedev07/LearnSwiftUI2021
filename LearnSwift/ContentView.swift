bfe29465343f3cf82b5af474a5656b71234be274
Tim <harvard90873@gmail.com>
2021-08-01 19:51:03 +0200

    Complete basic turtle rock page

--- LearnSwift/ContentView.swift
+++ LearnSwift/ContentView.swift
@@ -6,37 +6,45 @@
 //
 
 /**
- By default, SwiftUI view files declare two structures. The first structure conforms to the View protocol and describes the view’s content and layout. The second structure declares a preview for that view.
- */
+By default, SwiftUI view files declare two structures. The first structure conforms to the View protocol and describes the view’s content and layout. The second structure declares a preview for that view.
+*/
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        /**
-         In the preview, Command-click the greeting to bring up the structured editing popover, and choose “Show SwiftUI Inspector”.
-         */
-        VStack(alignment: .center) {
-          MapView().frame(height: 300)
-          CircleImage()
-          VStack(alignment: .leading) {
-              Text("Turtle Rock")
-                  .font(.title)
-              HStack {
-                  Text("Joshua Tree National Park")
-                      .font(.subheadline)
-                  Spacer()
-                  Text("California").font(.subheadline)
-              }
-          }
-          .padding()
+  var body: some View {
+    /**
+     In the preview, Command-click the greeting to bring up the structured editing popover, and choose “Show SwiftUI Inspector”.
+     */
+    VStack {
+      MapView().frame(height: 300).ignoresSafeArea(edges: .top)
+      CircleImage().offset(y: -110).padding(.bottom, -110)
+      VStack(alignment: .leading) {
+        Text("Turtle Rock")
+            .font(.title)
+        HStack {
+            Text("Joshua Tree National Park")
+                .font(.subheadline)
+            Spacer()
+            Text("California").font(.subheadline)
         }
-        
+        Divider()
+        Text("About Turtle Rock")
+            .font(.title2)
+        Text("Turtle Rock is a neighborhood in the south part of Irvine, Orange County, California, near Concordia University, Irvine and the University of California, Irvine.")
+      }
+      .padding()
+      
+      Spacer()
+      
+      
     }
+    
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView();
-    }
+  static var previews: some View {
+    ContentView();
+  }
 }
