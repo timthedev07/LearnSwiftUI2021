ba8734c2ee5939d893008f6d18979886cb226288
Tim <harvard90873@gmail.com>
2021-08-01 20:04:42 +0200

    custom landmark structure

--- LearnSwift/Landmark.swift
+++ LearnSwift/Landmark.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+// define a custom data structure
+struct Landmark: Hashable, Codable {
+
+    var id: Int
+
+    var name: String
+
+    var park: String
+
+    var state: String
+
+    var description: String
+
+}
