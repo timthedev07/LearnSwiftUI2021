ed3957dac88e34ea08943346d57af1591264a421
Tim <harvard90873@gmail.com>
2021-08-01 17:47:10 +0200

    initial

---
+++ LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by Tim on 2021/8/1.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
